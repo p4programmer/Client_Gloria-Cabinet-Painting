7acbf7c42550888db44497f50f940f5a72dcf02c
p4programmer <63483525+p4programmer@users.noreply.github.com>
2022-04-27 18:11:28 -0700

    Services page updated

--- services.php
+++ services.php
@@ -24,7 +24,169 @@ require_once("resource/config.php");
                 </div>
             </div>
         </div>
+
+        <div class="page-bottom margin-top-90">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="page-single">
+                            <div class="row margin-bottom-10">
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <h3 class="margin-bottom-20">Kitchen Cabinet Painting </h3>
+                                    <div class="margin-bottom-20">
+                                        <img src="assets/images/painting.jpg" class="img-fluid " alt="" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <div class="page-single-text">
+                                        <p>
+                                            Completely transform your old kitchen cabinets to new and modern looking
+                                            ones by painting them with high quality low VOC waterborne lacquer.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="page-bottom margin-top-30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="page-single">
+                            <div class="row margin-bottom-10">
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <h3 class="margin-bottom-20">Bathrooms & Laundry Cabinet Painting</h3>
+                                    <div class="margin-bottom-20">
+                                        <img src="assets/images/bathpaint.jpg" class="img-fluid " alt="" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <div class="page-single-text">
+                                        <p>
+                                            Not only transform the kitchen cabinets, we can also help to transfigure
+                                            your bathroom and laundry cabinets by using high quality low VOC waterborne
+                                            lacquer.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="page-bottom margin-top-30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="page-single">
+                            <div class="row margin-bottom-10">
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <h3 class="margin-bottom-20">Custom cabinet & shelving painting</h3>
+                                    <div class="margin-bottom-20">
+                                        <img src="assets/images/painting2.jpg" class="img-fluid " alt="" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <div class="page-single-text">
+                                        <p>
+                                            Any types of custom cabinets and shelves that are made up of wood can also
+                                            be transformed into a new look by painting high quality low VOC waterborne
+                                            lacquer.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="page-bottom margin-top-30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="page-single">
+                            <div class="row margin-bottom-10">
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <h3 class="margin-bottom-20">New hardware’s</h3>
+                                    <div class="margin-bottom-20">
+                                        <img src="assets/images/hadware.jpg" class="img-fluid " alt="" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <div class="page-single-text">
+                                        <p>
+                                            New soft close hinges can be installed for your cabinet doors. In addition
+                                            to that, most of the drawers can also be installed with soft closes. This
+                                            will ensure that your cabinets doors and drawers are never shut abrasively.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="page-bottom margin-top-30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="page-single">
+                            <div class="row margin-bottom-10">
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <h3 class="margin-bottom-20">Counter Tops</h3>
+                                    <div class="margin-bottom-20">
+                                        <img src="assets/images/counter.jpg" class="img-fluid " alt="" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <div class="page-single-text">
+                                        <p>
+                                            Most economical quartz countertops can be installed for your kitchen as well
+                                            as for bathrooms. Customers can select their choice of patterns from wide
+                                            variety of stone collections.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="page-bottom margin-top-30 margin-bottom-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="page-single">
+                            <div class="row margin-bottom-10">
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <h3 class="margin-bottom-20">Back Splash</h3>
+                                    <div class="margin-bottom-20">
+                                        <img src="assets/images/counter.jpg" class="img-fluid " alt="" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 align-self-center">
+                                    <div class="page-single-text">
+                                        <p>
+                                            Normal backsplash tiles as well as matching quartz countertop patterns can
+                                            be installed for your kitchen backsplash and bathrooms.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
+
 
 
     <?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
